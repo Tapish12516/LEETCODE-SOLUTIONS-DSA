a5127224ebf322773c16d442130d7734cc6ce44a
Tapish <tapish179@gmail.com>
2026-09-14 21:49:58 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.97%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -3,7 +3,7 @@ public:
     int search(vector<int>& nums, int target) {
         int low = 0 , high = nums.size()-1;
         while(low <= high){
-            int mid = low + (high-low)/2 ;
+            int mid = low + (high-low)/2;
             if(nums[mid] == target) return mid;
             else if (nums[mid] >= target) high = mid-1;
             else low = mid+1;
