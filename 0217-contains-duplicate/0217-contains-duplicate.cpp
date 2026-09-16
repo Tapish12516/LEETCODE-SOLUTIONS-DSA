91e96a92c2df13cad0966a0e434fb4d8437660ab
Tapish <tapish179@gmail.com>
2026-09-16 14:50:59 +0530

    Time: 78 ms (31.11%), Space: 111.4 MB (28.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums){
+        unordered_set<int> set;
+        for(int i =0 ; i<nums.size() ; i++){
+            if(set.find(nums[i]) != set.end()) return true;         //same TC and SC but a sligthlt efficient approach
+            set.insert(nums[i]);
+        }
+        return false;
+    }
+};
+
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_map<int,int> mp;
+//         for(int num : nums){
+//             mp[num]++;                                              // TC and SC = O(N)
+//             if(mp[num] > 1)                                         // build full map and then traverses again 
+//                 return true;
+//         }
+//         return false;
+//     }
+// };
