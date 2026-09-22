0c65ab20089fa3b4577b22267c293f36c7daf109
Tapish <tapish179@gmail.com>
2026-09-22 23:46:58 +0530

    Time: 587 ms (30.97%), Space: 40.4 MB (34.18%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -20,15 +20,13 @@ public:
 };
 
 
-
 // class Solution {
 // public:
 //     int countPrimes(int n) {
-//         if(n==0||n==1) return 0;
 //         int prime = 0;
 //         for(int i = 2 ; i < n ; i++) {
 //             bool isprime = true;
-//             for(int j = 2 ; j <=  sqrt(i) ; j++ ){       //O(n*root(n))
+//             for(int j = 2 ; j <=  sqrt(i) ; j++ ){
 //                 if( i%j == 0) {
 //                     isprime = false;
 //                     break;
