11c59dddda82752ebfff2bd8a621467997ad21f7
Tapish <tapish179@gmail.com>
2026-09-24 18:10:41 +0530

    Time: 0 ms (100%), Space: 32.4 MB (59.56%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-            int mn = *min_element(nums.begin(), nums.end());
-            int mx = *max_element(nums.begin(), nums.end());
-            unordered_set<int> st(nums.begin(), nums.end());
-            vector<int> ans;
-            for (int x = mn + 1; x < mx; x++) {
-                if (!st.count(x))  ans.push_back(x);
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        vector<int> ans;
+        for (int i = 0; i < n - 1; i++) {                               //better approach
+            for (int j = nums[i] + 1; j < nums[i + 1]; j++) {
+                ans.push_back(j);
             }
+        }
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//             int mn = *min_element(nums.begin(), nums.end());
+//             int mx = *max_element(nums.begin(), nums.end());
+//             unordered_set<int> st(nums.begin(), nums.end());
+//             vector<int> ans;
+//             for (int x = mn + 1; x < mx; x++) {
+//                 if (!st.count(x))  ans.push_back(x);
+//             }
+//         return ans;
+//     }
+// };
