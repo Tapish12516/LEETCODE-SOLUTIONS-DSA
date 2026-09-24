87330c0bd280ed59e14ebb20f3c74b114d58e6a7
Tapish <tapish179@gmail.com>
2026-09-24 17:55:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (20.9%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.length();
+        int ans = 0;
+        int pushes = 1;
+        while (n > 0) {
+            int take = min(8, n);
+            ans += take * pushes;
+            pushes++;
+            n -= take;
+        }
+        return ans;
+    }
+};
+// https://share.google/aimode/5OhVsGRBnQy5KqaTp
